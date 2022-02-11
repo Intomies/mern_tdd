b7b12c4babf92ff33b5d21875bb011e8c6971ad1
Intomies <valtteri.nummi@gmail.com>
2022-02-11 23:02:18 +0200

    Connected MessageApp to Express

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,19 @@
 import express from 'express'
 import 'dotenv/config'
+import MessageApp from './lib/model.js'
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send({val: 'Hello World'})
+const messageApp = new MessageApp('./lib/json/testMessages.json')
+
+app.get('/', async (req, res) => {
+    const result = messageApp.getAll()
+    res.json(result)
 })
 
-app.listen(port)
-console.log(`App running at http://localhost:${port}`)
+app.listen(port, () => { 
+    console.log(`App running at http://localhost:${port}`)
+})
 export default app;
 
--- backend/test/model.test.js
+++ backend/test/model.test.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import MessageApp from '../lib/json/model.js'
+import MessageApp from '../lib/model.js'
 
 describe ('app', () => {
     
